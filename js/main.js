aeb7e5c467000907704f2e85c98ff0dc71bf057c
Virtual Saajje <86907000+virtualsaajje@users.noreply.github.com>
2021-07-06 21:24:00 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const joinBtn = document.querySelector("button");
+joinBtn.addEventListener("click",()={
+  alert("Once you join, please rename yourself to INDEX_NO__NAME_WITH_INITIALS");
+  window.open("https://hasathcharu.com");
+});
