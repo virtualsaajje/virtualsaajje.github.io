8cc5fa9126bff0c91ab2ff69a494a4538d1bad42
Virtual Saajje <86907000+virtualsaajje@users.noreply.github.com>
2021-07-07 08:40:48 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const joinBtn = document.querySelector("button");
 joinBtn.addEventListener("click",()=>{
-  alert("Once you join, please rename yourself to INDEX_NO__NAME_WITH_INITIALS");
-  window.open("https://hasathcharu.com");
+//   alert("Once you join, please rename yourself to INDEX_NO__NAME_WITH_INITIALS");
+  alert("We will enable this button from 30 minutes before the event.");
+//   window.open("https://hasathcharu.com");
 });
