cb0ea561d163d745f8fbd623cbdf442983615e21
Virtual Saajje <86907000+virtualsaajje@users.noreply.github.com>
2021-07-08 21:59:48 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 const joinBtn = document.querySelector("button");
-joinBtn.addEventListener("click",()=>{
-//   alert("Once you join, please rename yourself to INDEX_NO__NAME_WITH_INITIALS");
-  alert("This button will be enabled 30 minutes before the event.");
-//   window.open("https://hasathcharu.com");
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,    
+    function(m,key,value) {
+      vars[key] = value;
+    });
+    return vars;
+}
+
+joinBtn.addEventListener("click", () => {
+	alert("This button will be enabled 30 minutes before the event.");
+	var fType = getUrlVars()["uname"];
+	if(fType){
+		window.open(`https://us04web.zoom.us/j/76805975364?pwd=bDA1OUFPeHJLNzA4NFJ3OGlIV2ZEUT09&uname=${fType}`);
+	}else{
+		window.open(`https://us04web.zoom.us/j/76805975364?pwd=bDA1OUFPeHJLNzA4NFJ3OGlIV2ZEUT09`);
+	}
 });
