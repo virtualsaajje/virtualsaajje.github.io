8663a6740ddd8e11aab27b565ce8e0014214de7c
Virtual Saajje <86907000+virtualsaajje@users.noreply.github.com>
2021-07-08 22:22:10 +0530

    Update and rename main2.js to main3.js

--- js/main3.js
+++ js/main3.js
@@ -1,4 +1,11 @@
 const joinBtn = document.querySelector("button");
+const uname = getUrlVars()["uname"];
+const name = document.getElementById("name");
+if(uname){
+	name.textContent = uname;
+}else{
+	name.textContent = "Everyone"
+}
 function getUrlVars() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,    
@@ -10,9 +17,8 @@ function getUrlVars() {
 
 joinBtn.addEventListener("click", () => {
 	alert("testing");
-	var fType = getUrlVars()["uname"];
-	if(fType){
-		window.open(`https://us04web.zoom.us/j/76805975364?pwd=bDA1OUFPeHJLNzA4NFJ3OGlIV2ZEUT09&uname=${fType}`);
+	if(uname){
+		window.open(`https://us04web.zoom.us/j/76805975364?pwd=bDA1OUFPeHJLNzA4NFJ3OGlIV2ZEUT09&uname=${uname}`);
 	}else{
 		window.open(`https://us04web.zoom.us/j/76805975364?pwd=bDA1OUFPeHJLNzA4NFJ3OGlIV2ZEUT09`);
 	}
